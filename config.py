04b54368c046dfac8929ab093878a87a2f9f5ea9
lolajibolo <lohalogogi@gmail.com>
2025-05-04 15:11:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WEREWOLF-DEMON/EsproMusicBotDemon",
+    "https://github.com/lolajibolo/lolajibolo.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
